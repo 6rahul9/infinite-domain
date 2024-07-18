ae83e39815e553128791d3e47a2b7fe72676bba4
Your Name <rahulmohature0421@gmail.com>
2024-07-18 12:35:37 +0530

    new codes added

---
+++ src/Games/Debug/Debug.js
@@ -0,0 +1,12 @@
+import Stats from './Stats.js'
+import UI from './UI.js'
+
+export default class Debug{
+    static instance 
+    static getInstance()
+    {
+        return Debug.instance
+    }
+
+    constructror
+}
