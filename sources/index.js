de5cac496fc619cbaa62808e61a19cfdeeb7b9fc
Rahul Mohuture <97466426+6rahul9@users.noreply.github.com>
2024-09-30 05:20:14 +0000

    added complete new source file

--- sources/index.js
+++ sources/index.js
@@ -1,8 +1,6 @@
-// import Game from '@/Game.js'
-// import Game from './src/Game/Game.js';
-import Game from '@/Game/Game.js'; // Adjust the path to the correct folder structure
+import Game from '@/Game.js'
 
+const game = new Game()
 
-const game = new game()
 if(game.view)
     document.querySelector('.game').append(game.view.renderer.instance.domElement)
