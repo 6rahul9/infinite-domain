68339360b6be96384531d76568ff619652de66b2
Rahul Mohuture <97466426+6rahul9@users.noreply.github.com>
2024-09-30 05:17:50 +0000

    change the file name

--- sources/index.js
+++ sources/index.js
@@ -1,5 +1,6 @@
-import Game from '@/Game.js'
+// import Game from '@/Game.js'
 // import Game from './src/Game/Game.js';
+import Game from '@/Game/Game.js'; // Adjust the path to the correct folder structure
 
 
 const game = new game()
